53e0f373f8fb904fa440a1408305efc2586112fc
Nikhil <nikhilingale1998@gmail.com>
2021-02-07 22:54:37 +0530

    Update Inheritance Test

--- src/com/github/nikingale/inheritance/TestAnimal.java
+++ src/com/github/nikingale/inheritance/TestAnimal.java
@@ -4,11 +4,21 @@ public class TestAnimal {
 
     public static void main(String[] args) {
         Animal lion = new Lion();
-
         lion.eat();
         lion.sleep();
         lion.roam();
         lion.makeNoise();
+
+        Animal deer = new Deer("grass");
+        deer.sleep();
+        deer.roam();
+        deer.makeNoise();
+
+        Animal wolf = new Wolf();
+        wolf.eat();
+        wolf.sleep();
+        wolf.roam();
+        wolf.makeNoise();
     }
 
 }
