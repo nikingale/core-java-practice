bea138be9d06747a81fbb5979488d0d7a73f031a
Nikhil <nikhilingale1998@gmail.com>
2021-02-28 22:15:52 +0530

    Add File-handling example

---
+++ src/com/github/nikingale/filehandling/Jukebox.java
@@ -0,0 +1,42 @@
+package com.github.nikingale.filehandling;
+
+import java.util.*;
+import java.io.*;
+
+/**
+ * @author Nikhil Ingale 05-04-2020
+ */
+
+public class Jukebox {
+
+    List<String> songList = new ArrayList<>();
+
+    public void play() {
+        getSongs();
+        for (String song : songList) {
+            System.out.println(song);
+        }
+    }
+
+    public void getSongs() {
+        try {
+            File file = new File("src/resources/SongList");
+            FileReader fileReader = new FileReader(file);
+            BufferedReader reader = new BufferedReader(fileReader);
+
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                addSong(line);
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void addSong(String line) {
+        String[] tokens = line.split("/");
+        songList.add(tokens[0]);
+    }
+
+}
---
+++ src/com/github/nikingale/filehandling/TestJukebox.java
@@ -0,0 +1,14 @@
+package com.github.nikingale.filehandling;
+
+/**
+ * @author Nikhil Ingale 05-04-2020
+ */
+
+public class TestJukebox {
+
+    public static void main(String[] args) {
+        Jukebox jukebox = new Jukebox();
+        jukebox.play();
+    }
+
+}
