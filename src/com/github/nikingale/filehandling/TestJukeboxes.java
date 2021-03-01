bed790255700e829f1f7b9084e313df9afcf12d6
Nikhil <nikhilingale1998@gmail.com>
2021-03-01 23:26:11 +0530

    Add Jukeboxes Test

--- src/com/github/nikingale/filehandling/TestJukeboxes.java
+++ src/com/github/nikingale/filehandling/TestJukeboxes.java
@@ -4,7 +4,7 @@ package com.github.nikingale.filehandling;
  * @author Nikhil Ingale 05-04-2020
  */
 
-public class TestJukebox {
+public class TestJukeboxes {
 
     public static void main(String[] args) {
         Jukebox jukebox = new Jukebox();
