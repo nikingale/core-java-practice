a2b4e64ad6a16c3bc68acb887c028672f30ea0ac
Nikhil <nikhilingale1998@gmail.com>
2021-03-25 22:44:16 +0530

    Add Java Application - Bank

--- src/com/github/nikingale/applications/bank/TestAccount.java
+++ src/com/github/nikingale/applications/bank/TestAccount.java
@@ -7,7 +7,7 @@ package com.github.nikingale.applications.bank;
 public class TestAccount {
 
     public static void main(String[] args) {
-        CreateAccount myAccount = new CreateAccount(101, "Nikhil Ingale", 100.0f, 123);
+        CreateAccount myAccount = new CreateAccount(101, "Nikhil Netaji Ingale", 100.0f, 123);
         myAccount.getMenu();
     }
 
