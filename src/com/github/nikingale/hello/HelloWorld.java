44d454f3e412ae5173c64df63c6aa4b44c766a71
nikingale <nikhilingale1998@gmail.com>
2024-02-03 17:03:34 +0530

    Change to World

--- src/com/github/nikingale/hello/HelloWorld.java
+++ src/com/github/nikingale/hello/HelloWorld.java
@@ -4,11 +4,11 @@ package com.github.nikingale.hello;
  * @author Nikhil Ingale 25-03-2020
  */
 
-public class HelloJava {
+public class HelloWorld {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello Java!");
+        System.out.println("Hello World!");
 
     }
 
