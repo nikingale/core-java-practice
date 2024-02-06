68aca6b64000104918e0e751f8ac4f7748260328
nikingale <nikhilingale1998@gmail.com>
2024-02-06 23:12:17 +0530

    Update scratchpad

--- src/com/github/nikingale/scratch/Scratch.java
+++ src/com/github/nikingale/scratch/Scratch.java
@@ -6,10 +6,5 @@ public class Scratch {
 
         Class1 obj1 = new Class1();
         obj1.printClass1Data();
-
-        System.out.println();
-        Class2 obj2 = new Class2();
-        obj2.printClass2Data();
-        System.out.println(obj2.addTwo(2, 5) );
     }
 }
