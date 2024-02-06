fec76c0b34cb26564956343949786b498329ec81
nikingale <nikhilingale1998@gmail.com>
2024-02-06 23:14:40 +0530

    Scratchpad

--- src/com/github/nikingale/scratch/Scratch.java
+++ src/com/github/nikingale/scratch/Scratch.java
@@ -2,7 +2,7 @@ package com.github.nikingale.scratch;
 
 public class Scratch {
     public static void main(String[] args) {
-        System.out.println("The scratch pad");
+        System.out.println("The Scratch Pad");
 
         Class1 obj1 = new Class1();
         obj1.printClass1Data();
