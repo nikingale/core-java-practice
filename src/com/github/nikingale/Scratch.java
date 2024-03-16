27e105a886ef9e8cb3affaa33436461390f86010
nikingale <nikhilingale1998@gmail.com>
2024-03-16 23:10:47 +0530

    Adds scratch class

--- src/com/github/nikingale/Scratch.java
+++ src/com/github/nikingale/Scratch.java
@@ -1,4 +1,34 @@
 package com.github.nikingale;
 
-public class Scratch {
+import java.util.List;
+
+public class scratch {
+
+    void display() {
+        System.out.println("Data " +data);
+    }
+    int data;
+    public static void main(String[] args) {
+        int x = 15;
+        scratch n = new scratch();
+        n.display();
+
+        int v = 25;
+        Integer b4 = v;
+        Long after = ++b4 ==26 ? 5: new Long(10);
+        System.out.println(after.intValue() - b4.intValue());
+
+        StringBuilder sb = new StringBuilder("buffering");
+        sb.replace(2,7, "BUFFER");
+        sb.delete(2, 4);
+        String s = sb.substring(1, 5);
+        System.out.println(s);
+
+        if (true) {
+            System.out.println("Hey");
+        }
+
+        Double d = null;
+        System.out.println((d instanceof Double) ? "true" : "f");
+    }
 }
