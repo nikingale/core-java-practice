3fd974c052a75c4caf7895593d756441e71fbafc
nikingale <nikhilingale1998@gmail.com>
2024-03-19 22:59:03 +0530

    Update streams example

--- src/com/github/nikingale/java8/stream/StreamAPI.java
+++ src/com/github/nikingale/java8/stream/StreamAPI.java
@@ -25,30 +25,30 @@ public class StreamAPI {
         names.add("Sujay");
         names.add("Ram");
 
-        System.out.println("Printing the collection : " + numbers);
+        System.out.println("Printing the collection: " + numbers);
 
         System.out.println();
         List<Integer> result = numbers.stream().filter(n -> n % 2 == 0).collect(Collectors.toList());
-        System.out.println("Even number stream list : " + result);
+        System.out.println("Even number stream list: " + result);
 
         List<Integer> mapResult = numbers.stream().map(n -> n + 10).collect(Collectors.toList());
-        System.out.println("Map result (number + 10) : " + mapResult);
+        System.out.println("Map result (number + 10): " + mapResult);
 
         List<Integer> sortedResult = numbers.stream().sorted().collect(Collectors.toList());
-        System.out.println("Sorted numbers result : " + sortedResult);
+        System.out.println("Sorted numbers result: " + sortedResult);
 
         Optional<Integer> maxNumber = numbers.stream().max(Integer::compareTo);
-        System.out.println("Max number is : " + maxNumber);
+        System.out.println("Max number is: " + maxNumber);
 
         System.out.println();
-        System.out.println("Printing the collection : " + names);
+        System.out.println("Printing the collection: " + names);
 
         System.out.println();
         List<String> filteredNames = names.stream().filter(s -> s.startsWith("S")).sorted().collect(Collectors.toList());
         System.out.println("List of names starting with S: " + filteredNames);
 
         String filteredNamesString = names.stream().filter(s -> s.startsWith("S")).sorted().collect(Collectors.joining("-"));
-        System.out.println("FilteredNamesString : " + filteredNamesString);
+        System.out.println("FilteredNamesString: " + filteredNamesString);
 
         List<Integer> mappedStream = names.stream().map(String::length).collect(Collectors.toList());
         System.out.println("Names length: " + mappedStream);
