e209ee02fec47a1c3d0b0b898ec7c52141661f84
nikingale <nikhilingale1998@gmail.com>
2024-03-20 23:06:54 +0530

    Update lambdas

--- src/com/github/nikingale/java8/lambda/GreetingLambda.java
+++ src/com/github/nikingale/java8/lambda/GreetingLambda.java
@@ -4,7 +4,7 @@ import java.util.Collections;
 
 public class GreetingLambda {
     public static void main(String[] args) {
-        Greeting myGreeting = name -> System.out.println("Hello " + name);
+        Greeting myGreeting = name -> System.out.println("Hello " + name + "!");
 
         myGreeting.greeting("Nikhil");
     }
